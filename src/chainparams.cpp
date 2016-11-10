shigeya/bitcion-bsafe-work

Create BSafe Network

src/chainparams.cpp
@@ -301,6 +301,67 @@ public:
 static CSegNetParams segNetParams;
 
 /**
+ * BSafe
+ */
+class CBSafeNetParams : public CChainParams {
+public:
+    CBSafeNetParams() {
+            strNetworkID = "bsafenet";
+            consensus.nSubsidyHalvingInterval = 210000;
+            consensus.nMajorityEnforceBlockUpgrade = 7;
+            consensus.nMajorityRejectBlockOutdated = 9;
+            consensus.nMajorityWindow = 10;
+            consensus.BIP34Height = -1;
+            consensus.BIP34Hash = uint256();
+            consensus.powLimit = uint256S("000001ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // 512x lower min difficulty than mainnet
+            consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
+            consensus.nPowTargetSpacing = 10 * 60;
+            consensus.fPowAllowMinDifficultyBlocks = true;
+            consensus.fPowNoRetargeting = false;
+            pchMessageStart[0] = 0xbe;
+            pchMessageStart[1] = 0x46;
+            pchMessageStart[2] = 0x9e;
+            pchMessageStart[3] = 0xc5;
+            nDefaultPort = 34821;
+            nPruneAfterHeight = 1000;
+            consensus.nRuleChangeActivationThreshold = 108; // 75% for testchains
+            consensus.nMinerConfirmationWindow = 144; // Faster than normal for segnet (144 instead of 2016)
+            consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
+            consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
+            consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
+            consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
+            consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 0;
+            consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 999999999999ULL;
+            consensus.vDeployments[Consensus::DEPLOYMENT_WITNESS].bit = 1;
+            consensus.vDeployments[Consensus::DEPLOYMENT_WITNESS].nStartTime = 0;
+            consensus.vDeployments[Consensus::DEPLOYMENT_WITNESS].nTimeout = 999999999999ULL;
+
+            genesis = CreateGenesisBlock(1464966958, 0, UintToArith256(consensus.powLimit).GetCompact(), 1, 50 * COIN);
+            consensus.hashGenesisBlock = genesis.GetHash();
+
+            vFixedSeeds.clear();
+            vSeeds.clear();
+
+            base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,30);
+            base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,50);
+            base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,158);
+            base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x05)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
+            base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x05)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
+
+            vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_bsafe, pnSeed6_bsafe + ARRAYLEN(pnSeed6_bsafe));
+
+            fMiningRequiresPeers = true;
+            fDefaultConsistencyChecks = false;
+            fRequireStandard = false;
+            fMineBlocksOnDemand = false;
+            fTestnetToBeDeprecatedFieldRPC = true;
+
+            // checkpointData is empty
+    }
+};
+static CBSafeNetParams bSafeParams;
+
+/**
  * Regression test
  */
 class CRegTestParams : public CChainParams {
@@ -382,6 +443,8 @@ CChainParams& Params(const std::string& chain)
             return testNetParams;
     else if (chain == CBaseChainParams::SEGNET)
             return segNetParams;
+    else if (chain == CBaseChainParams::BSAFENET)
+        return bSafeParams;
     else if (chain == CBaseChainParams::REGTEST)
             return regTestParams;
     else

keio-bcl/bitcoin

src/chainparams.cpp
@@ -241,6 +241,67 @@ public:
 static CTestNetParams testNetParams;
 
 /**
+ * BSafe
+ */
+class CBSafeNetParams : public CChainParams {
+public:
+    CBSafeNetParams() {
+            strNetworkID = "bsafenet";
+            consensus.nSubsidyHalvingInterval = 210000;
+            consensus.nMajorityEnforceBlockUpgrade = 7;
+            consensus.nMajorityRejectBlockOutdated = 9;
+            consensus.nMajorityWindow = 10;
+            consensus.BIP34Height = -1;
+            consensus.BIP34Hash = uint256();
+            consensus.powLimit = uint256S("000001ffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // 512x lower min difficulty than mainnet
+            consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
+            consensus.nPowTargetSpacing = 10 * 60;
+            consensus.fPowAllowMinDifficultyBlocks = true;
+            consensus.fPowNoRetargeting = false;
+            pchMessageStart[0] = 0xbe;
+            pchMessageStart[1] = 0x46;
+            pchMessageStart[2] = 0x9e;
+            pchMessageStart[3] = 0xc5;
+            nDefaultPort = 34821;
+            nPruneAfterHeight = 1000;
+            consensus.nRuleChangeActivationThreshold = 108; // 75% for testchains
+            consensus.nMinerConfirmationWindow = 144; // Faster than normal for segnet (144 instead of 2016)
+            consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].bit = 28;
+            consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nStartTime = 1199145601; // January 1, 2008
+            consensus.vDeployments[Consensus::DEPLOYMENT_TESTDUMMY].nTimeout = 1230767999; // December 31, 2008
+            consensus.vDeployments[Consensus::DEPLOYMENT_CSV].bit = 0;
+            consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nStartTime = 0;
+            consensus.vDeployments[Consensus::DEPLOYMENT_CSV].nTimeout = 999999999999ULL;
+            consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
+            consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1462060800;
+            consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1493596800;
+
+            genesis = CreateGenesisBlock(1464966958, 0, 0x1e01ffff, 1, 50 * COIN);
+            consensus.hashGenesisBlock = genesis.GetHash();
+
+            vFixedSeeds.clear();
+            vSeeds.clear();
+
+            base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,30);
+            base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,50);
+            base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,158);
+            base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x05)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
+            base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x05)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
+
+            vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_bsafe, pnSeed6_bsafe + ARRAYLEN(pnSeed6_bsafe));
+
+            fMiningRequiresPeers = true;
+            fDefaultConsistencyChecks = false;
+            fRequireStandard = false;
+            fMineBlocksOnDemand = false;
+            fTestnetToBeDeprecatedFieldRPC = true;
+
+            // checkpointData is empty
+    }
+};
+static CBSafeNetParams bSafeParams;
+
+/**
  * Regression test
  */
 class CRegTestParams : public CChainParams {
@@ -326,6 +387,8 @@ CChainParams& Params(const std::string& chain)
             return mainParams;
     else if (chain == CBaseChainParams::TESTNET)
             return testNetParams;
+    else if (chain == CBaseChainParams::BSAFENET)
+        return bSafeParams;
     else if (chain == CBaseChainParams::REGTEST)
             return regTestParams;
     else